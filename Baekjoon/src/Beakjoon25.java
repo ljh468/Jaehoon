c113a83c8e93630eb03808cbcf805248983e22ca
ljh468 <ljh468@naver.com>
2020-06-09 08:37:29 +0900

    200609

---
+++ Baekjoon/src/Beakjoon25.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Beakjoon25{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
+        while(s.hasNext()) {
+        	int a =  s.nextInt();
+            int b =  s.nextInt();
+            System.out.println(a+b);
+            
+            
+        }
+            
+    }
+}
+
---
+++ Map/src/MapEx.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class MapEx {
+
+	public static void main(String[] args) {
+		List<HashMap<String, String>> rList = new ArrayList<>();
+		HashMap <String, String> pMap = new HashMap<>();
+		
+		pMap.put("name","이재훈");
+		pMap.put("email","ljh@");
+		pMap.put("addr","안양");
+		pMap.put("dept","데분");
+		
+		rList.add(pMap);
+		pMap = null;
+		
+		pMap = new HashMap<>();
+		
+		pMap.put("name","홍길동");
+		pMap.put("email","hong@");
+		pMap.put("addr","서울");
+		pMap.put("dept","시각");
+		
+		rList.add(pMap);
+		pMap = null;
+		
+		Iterator<HashMap<String, String>> it = rList.iterator();
+		while(it.hasNext()) {
+			HashMap<String, String> rMap = it.next();
+			System.out.println(rMap.get("name"));
+			System.out.println(rMap.get("email"));
+			System.out.println(rMap.get("addr"));
+			System.out.println(rMap.get("dept"));
+			rMap = null;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

b9e1ed4edf60e75b9fec23453e318d751c226ab5
ljh468 <ljh468@naver.com>
2020-06-13 09:54:48 +0900

    200613

---
+++ Baekjoon/src/Baekjoon32.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Baekjoon32{
+    public static void main(String[] args){
+      	    /* Scanner s = new Scanner(System.in);
+    	       int sum = 0;
+    	       int arr = 0;
+    	       for(int i=0; i<5; i++) {
+    	    	   arr = s.nextInt();
+    	    	   if(arr<40) {
+    	    		   arr = 40;
+    	    	   }
+    	    	   sum += arr;
+    	       }
+    	       System.out.println(sum/5); */
+
+    	Scanner s = new Scanner(System.in);
+    	int[] arr = new int[5];
+    	int i = 0;
+    	int sum = 0;
+    	for(i=0; i<5; i++) {
+    		arr[i] = s.nextInt();
+    		if(arr[i]<40) {
+    			arr[i]=40;
+    		}
+    	
+    	sum += arr[i];
+    	}
+    	int avg = sum/5;
+    	System.out.println(avg);
+    }
+}
+

5913101949730a663ada70bd2a51c152178f137d
ljh468 <ljh468@naver.com>
2020-06-14 23:25:05 +0900

    200615

--- Baekjoon/src/Baekjoon28.java
+++ Baekjoon/src/Baekjoon28.java
@@ -1,23 +1,18 @@
-// x보다 작은수 출력 (n개로 이루어진 배열, for, if이용)
+// 세수 입력, 중간수 찾기 (sort정렬 함수 이용)
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Baekjoon28 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int x = s.nextInt();
-		
-		int[] arr = new int[n];
-		for(int i = 0; i<arr.length; i++) {
+		int[] arr = new int[3];
+		int i=0;
+		while(i<arr.length) {
 			arr[i] = s.nextInt();
+			i++;
 		}
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
 		
-		for(int i = 0; i<arr.length; i++) {
-			if(arr[i]<x) {
-				System.out.printf("%d ",arr[i]);
-			}
-		}
-		
-
 	}
 }
---
+++ Baekjoon/src/Baekjoon34.java
@@ -0,0 +1,25 @@
+//상근날드 입력받은값중 최소값찾기
+import java.util.Scanner;
+
+public class Baekjoon34 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int buger = 2001;
+		int drink = 2001;
+
+		for (int i = 0; i < 3; i++) {
+			int bg = s.nextInt();
+			if (bg < buger) {
+				buger = bg;
+			}
+		}
+
+		for (int i = 0; i < 2; i++) {
+			int dr = s.nextInt();
+			if (dr < drink) {
+				drink = dr;
+			}
+		}
+		System.out.println((buger + drink) - 50);
+	}
+}

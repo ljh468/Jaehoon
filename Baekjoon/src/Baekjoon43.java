10f0f603abf34ae55633306c49a2738f0470dd96
ljh468 <ljh468@naver.com>
2020-06-26 00:31:54 +0900

    200626

---
+++ Baekjoon/src/Baekjoon43.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+public class Baekjoon43 {
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+ 
+        int num = s.nextInt();
+        s.nextLine(); //  개행문자 제거.. 
+        
+        
+        String[] str = new String[num];
+ 
+        for (int i = 0; i < num; i++) {
+        	str[i] = s.nextLine();   //  nextLine은 한라인을 전체 읽는다
+        	int count = 0;
+            int sum = 0;         
+   
+            for (int j = 0; j < str[i].length(); j++) {
+                if (str[i].charAt(j) == 'O') {
+                	count++;
+                    sum += count;
+                }
+                else count = 0;
+            }
+            System.out.println(sum);
+        }
+        
+        s.close();
+    }
+}

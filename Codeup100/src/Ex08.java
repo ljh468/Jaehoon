df0be9e6e8f393c9d371a2f96d86ae0496be69c8
ljh468 <ljh468@naver.com>
2020-06-28 03:08:32 +0900

    200628

---
+++ Codeup100/src/Ex08.java
@@ -0,0 +1,13 @@
+public class Ex08 {
+ 
+    public static void main(String[] str) {
+                
+        System.out.println("\u250C\u252C\u2510");
+        
+        System.out.println("\u251C\u253C\u2524");
+ 
+        System.out.println("\u2514\u2534\u2518");
+ 
+    }
+ 
+}

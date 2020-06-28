34375fb5b3a55b26802a43afd82ecdc3d43b6277
ljh468 <ljh468@naver.com>
2020-06-29 02:02:00 +0900

    200629

---
+++ Codeup100/src/Ex09.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex09 {
+ 
+    public static void main(String[] str) {
+                
+        Scanner s = new Scanner(System.in);
+        
+        int i = s.nextInt();
+        System.out.println(i);
+    }
+ 
+}

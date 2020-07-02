af4e6c93031dbc6effdda08eff6797393b53236a
ljh468 <ljh468@naver.com>
2020-07-02 20:57:56 +0900

    200702

--- Exam/src/Ex00.java
+++ Exam/src/Ex00.java
@@ -2,18 +2,17 @@
 public class Ex00 {
 
 	public static void main(String[] args) {
-		int i;
-		int k;
+		/*
+		 * int i; int k;
+		 * 
+		 * for(i=2; i<10; i++) { if(i==3||i==5) { System.out.println("<"+i+"´Ü>"); }
+		 * for(k=1; k<10; k++) { System.out.println(i+"*"+k+"="+i*k); }
+		 * System.out.println(); }
+		 */
+		int a= 110;
+		float b = 110.6f;
+		System.out.printf("%d",(int)b);
 		
-		for(i=2; i<10; i++) {
-			if(i==3||i==5) {
-				System.out.println("<"+i+"´Ü>");
-			}
-			for(k=1; k<10; k++) {
-				System.out.println(i+"*"+k+"="+i*k);
-			}
-			System.out.println();
-		}
 	}
 
 }

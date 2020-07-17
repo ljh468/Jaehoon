213f75fe64362c0a31a34ca1e1c321165e15e5c8
ljh468 <ljh468@naver.com>
2020-07-17 14:48:03 +0900

    200717

--- Baekjoon/src/Baekjoon46.java
+++ Baekjoon/src/Baekjoon46.java
@@ -1,6 +1,7 @@
 // 셀프넘버
 public class Baekjoon46 {
 	public static void main(String[] args) {
+		//10000이하에서 찾아야 함 
 		int limit = 10000;
 		boolean[] temp = new boolean[limit];
 		for (int i = 1; i < limit; i++) {
@@ -8,6 +9,7 @@ public class Baekjoon46 {
 				temp[d(i) - 1] = true;
 			}
 		}
+		//셀프넘버를 출력
 		for (int j = 0; j < 10000; j++) {
 			if (!temp[j]) {
 				System.out.println(j + 1);
@@ -15,13 +17,13 @@ public class Baekjoon46 {
 		}
 
 	}
-
+	// 셀프넘버를 찾기위한 함수 (n으로 d를 찾음
 	public static int d(int n) {
-		int num = n;
+		int d = n;
 		while (n > 0) {
-			num += n % 10;
+			d += n % 10;
 			n /= 10;
 		}
-		return num;
+		return d;
 	}
 }

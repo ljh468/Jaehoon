a8277f82290c39667133db6ef7d335301bd82394
ljh468 <ljh468@naver.com>
2020-07-23 00:31:09 +0900

    200723

---
+++ Codeup100/src/Ex29.java
@@ -0,0 +1,12 @@
+// 문자를 입력받고 10진수로 출력
+import java.util.Scanner;
+
+public class Ex29 {
+ 
+    public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+    	String a = s.nextLine();
+    	int n = a.charAt(0);
+    	System.out.println(n);
+    	}
+    }

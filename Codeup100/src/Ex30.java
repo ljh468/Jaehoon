8cdc593441b500a200df2a264239dc74ccf28364
ljh468 <ljh468@naver.com>
2020-07-24 00:06:37 +0900

    200724

---
+++ Codeup100/src/Ex30.java
@@ -0,0 +1,12 @@
+// 정수를 입력받아 아스키코드로 출력
+import java.util.Scanner;
+
+public class Ex30 {
+ 
+    public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+    	int a = s.nextInt();
+    	char b = (char)a;
+    	System.out.println(b);
+    	}
+    }

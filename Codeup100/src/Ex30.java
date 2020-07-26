a141fd68e637dea6b98bbbfa6f54475cd93cb486
ljh468 <ljh468@naver.com>
2020-07-27 04:27:30 +0900

    200727

--- Codeup100/src/Ex30.java
+++ Codeup100/src/Ex30.java
@@ -1,5 +1,19 @@
-// 정수를 입력받아 아스키코드로 출력
 import java.util.Scanner;
+
+public class Ex30{
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		long a = s.nextLong();
+		long b = s.nextLong();
+		System.out.println(a + b);
+	}
+}
+
+
+
+// 정수를 입력받아 아스키코드로 출력
+/* import java.util.Scanner;
 
 public class Ex30 {
  
@@ -10,3 +24,4 @@ public class Ex30 {
     	System.out.println(b);
     	}
     }
+*/

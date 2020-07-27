65fa6bbde6165645e58897ba2881416f336496ba
ljh468 <ljh468@naver.com>
2020-07-28 00:24:48 +0900

    200728

---
+++ Codeup100/src/Ex31.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex31{
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		char a = s.next().charAt(0);
+		
+		System.out.println((char)(a+1));
+	}
+}
+
+/*
+ import java.util.Scanner;
+
+public class Ex31{
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		long a = s.nextLong();
+		long b = s.nextLong();
+		System.out.println(a + b);
+	}
+}
+ */
---
+++ Codeup100/src/Ex32.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex32{
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		
+		System.out.println(a%b);
+	}
+}
+
+/*
+ * import java.util.Scanner;
+ * 
+ * public class Ex32{ public static void main(String[] args) {
+ * 
+ * Scanner s = new Scanner(System.in); int a = s.nextInt(); int b = s.nextInt();
+ * 
+ * 
+ * System.out.println(a/b); } }
+ */
+
+

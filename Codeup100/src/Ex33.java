6350e5c92383e22f2a0606e870404b6cffa4e787
ljh468 <ljh468@naver.com>
2020-07-29 05:22:32 +0900

    200729

---
+++ Codeup100/src/Ex33.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex33{
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		long a = s.nextLong();
+		
+		System.out.println(++a);
+	}
+}

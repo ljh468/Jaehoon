f814a2363246cb308acbf0574db6715eb1d7a344
ljh468 <ljh468@naver.com>
2020-08-07 02:24:51 +0900

    200807

---
+++ String/src/StringBuilder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class StringBuilder {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = scan.nextInt();
+        for(int i=1 ; i<=N ; i++) {
+            sb.append(i + "\n");
+        }
+        
+        System.out.println(sb);
+        
+        scan.close();
+    }
+ 
+}
+
+

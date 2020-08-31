e8e34db89379db90cb5b1c1c02eb381813af6e58
ljh468 <ljh468@naver.com>
2020-09-01 00:48:43 +0900

    200901

--- Codeup100/src/Ex36.java
+++ Codeup100/src/Ex36.java
@@ -1,15 +1,30 @@
-// 정수 2개를 입력받아 거듭제곱 배로 출력하기
+// 두 정수 입력받아 비교하기
 import java.util.Scanner;
 
 public class Ex36{
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int num = s.nextInt();
+		int num1 = s.nextInt();
 		int num2 = s.nextInt();
 		s.close();
 		
-		 //정수 a의 각 비트를 b만큼 왼쪽으로 이동(빈자리는 0으로 채워진다)
-        System.out.println(num<<num2);
+		if(num1>num2) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
 	}
 }
 
+/*
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+int num = s.nextInt();
+int num2 = s.nextInt();
+s.close();
+
+ // 정수 a의 각 비트를 b만큼 왼쪽으로 이동(빈자리는 0으로 채워진다)
+ // num 을 2의num2만큼 곱한값 출력
+System.out.println(num<<num2);
+}
+*/

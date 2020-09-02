0fafef10016f63d0ab9c8bed8d55e47e926af8ba
ljh468 <ljh468@naver.com>
2020-09-03 04:51:29 +0900

    200903

--- Codeup100/src/Ex37.java
+++ Codeup100/src/Ex37.java
@@ -1,4 +1,4 @@
-// 두 정수 입력받아 비교하기
+// 두 정수 입력받아 비교하기2
 import java.util.Scanner;
 
 public class Ex37{
@@ -8,10 +8,43 @@ public class Ex37{
 		int num2 = s.nextInt();
 		s.close();
 		
-		if(num1==num2) {
+		if(num1<=num2) {
 			System.out.println(1);
 		}else {
 			System.out.println(0);
 		}
 	}
 }
+
+/* 두 정수 입력받아 비교하기3
+public class Ex37{
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int num1 = s.nextInt();
+	int num2 = s.nextInt();
+	s.close();
+	
+	if(num1==num2) {
+		System.out.println(1);
+	}else {
+		System.out.println(0);
+	}
+}
+}
+*/
+/* 정수2개 입력받아서 비교 4
+public class Ex37{
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int num1 = s.nextInt();
+	int num2 = s.nextInt();
+	s.close();
+	
+	if(num1!=num2) {
+		System.out.println(1);
+	}else {
+		System.out.println(0);
+	}
+}
+}
+*/
---
+++ Codeup100/src/Ex38.java
@@ -0,0 +1,11 @@
+// 참거짓 논리바꾸기
+import java.util.Scanner;
+
+public class Ex38 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+
+		System.out.println(a == 1 ? 0 : 1); // a가 1이면 0 아니면 1;;
+	}
+}

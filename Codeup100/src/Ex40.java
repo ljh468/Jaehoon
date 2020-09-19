91bf67741b13b5afede45abc950259c2bad8f1ef
ljh468 <ljh468@naver.com>
2020-09-19 20:49:18 +0900

    200919

---
+++ Codeup100/src/Ex40.java
@@ -0,0 +1,18 @@
+// 참(1), 거짓(0) 이서로 다를때 1출력
+import java.util.Scanner;
+
+public class Ex40{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+       if(a!=1&&b==1||a==1&&b!=1) {
+           System.out.print(1);
+       }else {
+           System.out.print(0);
+       }
+       
+    }
+}

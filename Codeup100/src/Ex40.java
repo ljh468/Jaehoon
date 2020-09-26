80a4a1f9b1e9783d78c9bd5ecd31ff320f7bb073
ljh468 <ljh468@naver.com>
2020-09-26 22:43:08 +0900

    200926

--- Codeup100/src/Ex40.java
+++ Codeup100/src/Ex40.java
@@ -1,18 +1,33 @@
-// 참(1), 거짓(0) 이서로 다를때 1출력
+// 참(1), 거짓(0) 서로 같을때 1출력
 import java.util.Scanner;
 
 public class Ex40{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-       if(a!=1&&b==1||a==1&&b!=1) {
-           System.out.print(1);
-       }else {
-           System.out.print(0);
-       }
-       
-    }
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		if(a==1&&b==1||a==0&&b==0) {
+		   System.out.print(1);
+		}else {
+		   System.out.print(0);
+		}
+
+		}
 }
+/* 참(1), 거짓(0) 이서로 다를때 1출력
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+
+int a = sc.nextInt();
+int b = sc.nextInt();
+
+if(a!=1&&b==1||a==1&&b!=1) {
+   System.out.print(1);
+}else {
+   System.out.print(0);
+}
+
+}
+*/

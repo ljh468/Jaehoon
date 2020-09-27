a5db3270ed507c15cc804388cbde94127b2aff3e
ljh468 <ljh468@naver.com>
2020-09-27 20:39:54 +0900

    200927

---
+++ Codeup100/src/Ex41.java
@@ -0,0 +1,18 @@
+// 두 개의 참(1) 또는 거짓(0)이 입력될 때, 모두 거짓일 때에만 참이 계산
+import java.util.Scanner;
+
+public class Ex41{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		if(a==0&&b==0) {
+		   System.out.print(1);
+		}else {
+		   System.out.print(0);
+		}
+
+		}
+}

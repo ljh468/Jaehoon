de792e7c7036d95a2de8a12d82221dc26ebb90f1
ljh468 <ljh468@naver.com>
2020-09-27 23:11:08 +0900

    200927

--- Codeup100/src/Ex41.java
+++ Codeup100/src/Ex41.java
@@ -1,18 +1,30 @@
-// 두 개의 참(1) 또는 거짓(0)이 입력될 때, 모두 거짓일 때에만 참이 계산
+
 import java.util.Scanner;
 
-public class Ex41{
-	public static void main(String[] args){
+public class Ex41 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = ~a;
+		
+		System.out.println(b);
+
+	}
+}
+//두 개의 참(1) 또는 거짓(0)이 입력될 때, 모두 거짓일 때에만 참이 계산
+/*
+public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 
-		if(a==0&&b==0) {
-		   System.out.print(1);
-		}else {
-		   System.out.print(0);
+		if (a == 0 && b == 0) {
+			System.out.print(1);
+		} else {
+			System.out.print(0);
 		}
 
-		}
-}
+	}
+*/

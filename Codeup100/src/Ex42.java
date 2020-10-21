45e6dee4f18a961e7b192a1254686f467504149e
ljh468 <ljh468@naver.com>
2020-10-21 23:45:38 +0900

    201021

---
+++ Codeup100/src/Ex42.java
@@ -0,0 +1,15 @@
+// 비트단위 연산으로 and계산
+// and는 논리 곱
+import java.util.Scanner;
+
+public class Ex42 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a&b);
+
+	}
+}

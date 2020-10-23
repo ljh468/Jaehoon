8c97e8e7a8852d76063af08c9c804f400ca48eb6
ljh468 <ljh468@naver.com>
2020-10-23 23:39:16 +0900

    201023

--- Codeup100/src/Ex42.java
+++ Codeup100/src/Ex42.java
@@ -9,7 +9,22 @@ public class Ex42 {
 		int a = s.nextInt();
 		int b = s.nextInt();
 		
+		System.out.println(a|b);
+
+	}
+}
+
+
+/*
+public class Ex42 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
 		System.out.println(a&b);
 
 	}
 }
+*/

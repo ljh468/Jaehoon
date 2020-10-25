5e1c42be90fb0e62562d45a584349b44a853e982
ljh468 <ljh468@naver.com>
2020-10-25 23:47:15 +0900

    201025

---
+++ Codeup100/src/Ex43.java
@@ -0,0 +1,16 @@
+// 비트단위 연산으로 XOR계산
+import java.util.Scanner;
+
+public class Ex43 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a^b);
+
+	}
+}
+
+

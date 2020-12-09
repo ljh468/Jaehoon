f737f3ae6d1ac041757d3ef23be447f7d41b06a9
ljh468 <ljh468@naver.com>
2020-12-09 23:01:27 +0900

    201209

--- Codeup100/src/Ex44.java
+++ Codeup100/src/Ex44.java
@@ -1,3 +1,30 @@
+// a,b,c 중 짝수값 출력
+import java.util.Scanner;
+
+public class Ex44 {
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	int b = s.nextInt();
+	int c = s.nextInt();
+	
+	if(a % 2 == 0) {
+		System.out.printf("%d ",a);
+		
+	}
+	if(b % 2 == 0) {
+		System.out.printf("%d ",b);
+		
+	}
+	if(c % 2 == 0) {
+		System.out.printf("%d ",c);
+		
+	}
+	
+	}
+}
+
+/*
 // a,b,c 중 큰값 출력 3항 연산자
 import java.util.Scanner;
 
@@ -11,4 +38,4 @@ public class Ex44 {
 	System.out.println((a < b ? a : b) < c ? (a < b ? a : b) : c);
 	}
 }
-
+*/

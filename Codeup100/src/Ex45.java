4ee1877a56ff8025794bdba3164bf5aed34e23b8
ljh468 <ljh468@naver.com>
2020-12-11 14:50:36 +0900

    201211

---
+++ Codeup100/src/Ex45.java
@@ -0,0 +1,29 @@
+// 세 정수 a, b, c가 입력되었을 때, 짝(even)/홀(odd)을 출력해보자.
+import java.util.Scanner;
+
+public class Ex45 {
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	int b = s.nextInt();
+	int c = s.nextInt();
+	
+	if(a%2 == 0)
+		System.out.println("even");
+	else if(a%2 == 1)
+	System.out.println("odd");
+	
+	if(b%2 == 0)
+		System.out.println("even");
+	else if(b%2 == 1)
+		System.out.println("odd");
+	
+	if(c%2 == 0)
+		System.out.println("even");
+	else if(c%2 == 1)
+		System.out.println("odd");
+	
+}
+	
+	}
+

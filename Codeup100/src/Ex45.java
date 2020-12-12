86f2feffdd7b54765c4f06e158042a853d0d8d8a
ljh468 <ljh468@naver.com>
2020-12-12 19:28:17 +0900

    201212

--- Codeup100/src/Ex45.java
+++ Codeup100/src/Ex45.java
@@ -1,6 +1,27 @@
-// 세 정수 a, b, c가 입력되었을 때, 짝(even)/홀(odd)을 출력해보자.
+
+// 정수 1개가 입력되었을 때, 음(minus)/양(plus)과 짝(even)/홀(odd)을 출력해보자.
 import java.util.Scanner;
 
+public class Ex45 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+       
+		if(a > 0){
+		    System.out.println("plus");
+		}else{
+		    System.out.println("minus");
+		}
+        if(a%2 ==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+	}
+}
+
+/* // 세 정수 a, b, c가 입력되었을 때, 짝(even)/홀(odd)을 출력해보자.
 public class Ex45 {
 	public static void main(String[] args) {
 	Scanner s = new Scanner(System.in);
@@ -26,4 +47,5 @@ public class Ex45 {
 }
 	
 	}
+*/
 

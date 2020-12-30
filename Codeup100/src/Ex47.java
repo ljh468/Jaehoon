43e349b22c08feb44ae51dee5bff530a38c1496e
ljh468 <ljh468@naver.com>
2020-12-30 22:04:27 +0900

    20123-0

--- Codeup100/src/Ex47.java
+++ Codeup100/src/Ex47.java
@@ -1,6 +1,38 @@
-// 평가를 문자(A, B, C, D, ...)로 입력받아 내용을 다르게 출력해보자.
 import java.util.Scanner;
+public class Ex47 {
+        public static void main(String[] args) {
+            
+            Scanner s = new Scanner(System.in);
+            int mon = s.nextInt();
+            
+            switch(mon) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("fall");
+                break;
+                default:
+        }
+    }
+}
 
+//평가를 문자(A, B, C, D, ...)로 입력받아 내용을 다르게 출력해보자.
+/*
 public class Ex47 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -24,3 +56,4 @@ public class Ex47 {
 		}
 	}
 }
+*/

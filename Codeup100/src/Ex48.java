83b83f606b21356b4134275c09b2a734a43d6c45
ljh468 <ljh468@naver.com>
2021-01-01 15:12:52 +0900

    210101

--- Codeup100/src/Ex48.java
+++ Codeup100/src/Ex48.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
 
+//  정수 입력받아 계속 출력하기
 public class Ex48 {
 	public static void main(String[] args) {
+
 		Scanner s = new Scanner(System.in);
-		
-		while(true) {
-			int num = s.nextInt();
-			
-			if (num == 0) {
-				break;
-			}
-			System.out.println(num);
+		int size = s.nextInt();
+
+		for (int i = 0; i < size; i++) {
+			int a = s.nextInt();
+			System.out.println(a);
 		}
 	}
 }
+
+/*
+ * 0 입력될 때까지 무한 출력하기1 
+ * public static void main(String[] args) { 
+ * Scanner s = new Scanner(System.in);
+ * 
+ * while(true) { 
+ * int num = s.nextInt();https://codeup.kr/d_thread.php?tid=5530&cid=2068
+ * if (num == 0) { 
+ * 		break; 
+ * } 
+ * System.out.println(num); }
+ */

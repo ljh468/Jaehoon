1cf84e914a04f3dd9554e5857849152014999660
ljh468 <ljh468@naver.com>
2021-01-03 18:49:21 +0900

    210103

--- Codeup100/src/Ex49.java
+++ Codeup100/src/Ex49.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
+// 정수 1개 입력받아 카운트다운 출력하기1(설명)
+public class Ex49 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		while(true) {
+		
+		if (num == 0) {
+			break;
+		}
+		System.out.println(num);
+		num--;
+		}
+	}
+}
+
+/*
 // 0 입력될 때까지 무한 출력하기2
 public class Ex49 {
 	public static void main(String[] args) {
@@ -14,3 +31,4 @@ public class Ex49 {
 		}
 	}
 }
+*/

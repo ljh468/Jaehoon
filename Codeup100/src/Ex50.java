25be601a6bf74e6036a77d755f5230119cd97045
ljh468 <ljh468@naver.com>
2021-01-09 16:20:03 +0900

    210109

--- Codeup100/src/Ex50.java
+++ Codeup100/src/Ex50.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
+// 문자 1개 입력받아 알파벳 출력하기(설명)
+public class Ex50 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		char n = s.nextLine().charAt(0);
+		
+		char a = 'a';
+		
+		while(a<=n) {
+			System.out.print(a+" ");
+			a++;
+		}
+		
+	}
+}
 
+
+/*
 // 정수 1개 입력받아 카운트다운 출력하기2(설명)
 public class Ex50 {
 	public static void main(String[] args) {
@@ -16,4 +33,5 @@ public class Ex50 {
 
 		}
 	}
-}
+} 
+ */

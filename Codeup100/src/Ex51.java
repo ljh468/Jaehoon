58c1dc68e942cf879c5badba68238d963abec161
ljh468 <ljh468@naver.com>
2021-01-13 10:53:39 +0900

    210113

--- Codeup100/src/Ex51.java
+++ Codeup100/src/Ex51.java
@@ -1,4 +1,26 @@
 import java.util.Scanner;
+
+// 짝수의 합 구히기
+public class Ex51{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1;
+		int hap = 0;
+		while(i <= n) {
+			
+			if(i%2==0) {
+				hap += i;	
+			}
+			i++;
+		}
+		System.out.println(hap);
+	}
+}
+
+
+
+/*
 // 정수 1개 입력받아 그 수까지 출력하기(설명)
 public class Ex51 {
 	public static void main(String[] args) {
@@ -16,3 +38,4 @@ public class Ex51 {
 }
 
 
+*/

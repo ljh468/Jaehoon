612b8343d867ca892f1e803e685c0b070a334583
ljh468 <ljh468@naver.com>
2021-01-15 23:03:22 +0900

    210115

--- Codeup100/src/Ex52.java
+++ Codeup100/src/Ex52.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
-// 원하는 문자가 입력될 때까지 반복 출력하기
+
+//[기초-종합] 언제까지 더해야 할까?
+public class Ex52 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		int sum = 0;
+		for (int i = 1; i <= num; i++) {
+			sum = sum + i;
+			if (sum >= num) {
+				System.out.print(i);
+				break;
+			}
+		}
+	}
+}
+
+/*
+//원하는 문자가 입력될 때까지 반복 출력하기
 public class Ex52 {
 	public static void main(String[] args) {
 
@@ -17,3 +35,4 @@ public class Ex52 {
 		}
 	}
 }
+*/

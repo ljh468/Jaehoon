49e07ee6510c6b829c29cd5c9446e8d7cab8be0c
ljh468 <ljh468@naver.com>
2021-01-17 07:17:39 +0900

    210117

--- Codeup100/src/Ex53.java
+++ Codeup100/src/Ex53.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
+// [기초-종합] 16진수 구구단?
+	public class Ex53 {
+		public static void main(String[] args) {
+			
+			Scanner s =new Scanner(System.in);
+			
+			String  a = s.nextLine();
+			
+			int num = Integer.parseInt(a,16);
+			
+			 for(int i=1; i<16; i++) {
+	                
+	                System.out.format("%X*%X=%X%n",num,i,num*i);
+	            }
+		}
+	}
 
+
+/*
 // [기초-종합] 주사위를 2개 던지면?(설명)
 public class Ex53 {
 	public static void main(String[] args) {
@@ -15,3 +33,4 @@ public class Ex53 {
 		}
 	}
 }
+*/

64888ff714e679a5147de2e02795a4a9149e0d20
ljh468 <ljh468@naver.com>
2021-01-24 06:19:44 +0900

    210124

--- Codeup100/src/Ex55.java
+++ Codeup100/src/Ex55.java
@@ -1,5 +1,43 @@
-import java.util.Scanner;
-// [기초-종합] 빛 섞어 색 만들기(설명)
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+//[기초-종합] 빛 섞어 색 만들기(설명)
+public class Ex55 {
+
+	public static void main(String[] args) {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+		try {
+			String[] n = br.readLine().split(" ");
+			int count = 0;
+			
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			for (int i = 0; i < Integer.valueOf(n[0]); i++) {
+				
+				for (int j = 0; j < Integer.valueOf(n[1]); j++) {
+					
+					for (int k = 0; k < Integer.valueOf(n[2]); k++) {
+						
+						bw.write(i + " " + j + " " + k + "\n");
+						count++;
+					}
+				}
+			}
+			bw.write(String.valueOf(count));
+			bw.flush();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}//end of main()
+}
+
+/* 시간초과
 public class Ex55{
 
 	public static void main(String[] args) {
@@ -25,4 +63,5 @@ public class Ex55{
 		System.out.println(count);
 	}
 }
+*/
 

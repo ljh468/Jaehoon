71b9fa96fde767340bf076da437cc84019e58298
ljh468 <ljh468@naver.com>
2021-01-26 16:58:53 +0900

    210126

--- Codeup100/src/Ex56.java
+++ Codeup100/src/Ex56.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
 
+// 그림 파일 저장용량 계산하기(설명)
 public class Ex56 {
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 
-		long h = s.nextLong();
+		long r = s.nextLong();
+		long g = s.nextLong();
 		long b = s.nextLong();
-		long c = s.nextLong();
-		long d = s.nextLong();
-		double total = 0;
 
-		total = h * b * c * d;
+		double f = (double) r * g * b / 8 / 1024 / 1024;
 
-		double result = ((total / 8) / Math.pow(2, 10) / Math.pow(2, 10));
-		System.out.format("%.1f MB", result);
+		System.out.printf("%.2f MB", f);
 	}
+
 }
+
+/*
+ * // 그림 파일 저장용량 계산하기(설명) public class Ex56 { public static void main(String[]
+ * args) { Scanner s = new Scanner(System.in);
+ * 
+ * long h = s.nextLong(); 
+ * long b = s.nextLong(); 
+ * long c = s.nextLong(); 
+ * long d = s.nextLong(); 
+ * double total = 0;
+ * 
+ * total = h * b * c * d;
+ * 
+ * double result = ((total / 8) / Math.pow(2, 10) / Math.pow(2, 10));
+ * System.out.format("%.1f MB", result); } }
+ */

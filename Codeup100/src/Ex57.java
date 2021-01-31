b857c862d3e84ced761b3df2f9fd02e95ca3c00c
ljh468 <ljh468@naver.com>
2021-01-31 21:24:20 +0900

    210131

--- Codeup100/src/Ex57.java
+++ Codeup100/src/Ex57.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
+// [기초-종합] 3의 배수는 통과?
+public class Ex57{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        
+        for(int i =1; i<=a; i++) {
+        	if(i%3==0)continue;
+        	System.out.printf("%d ",i);
+        }
+	}
+}
+
+
+/*
 // [기초-종합] 여기까지! 이제 그만
 public class Ex57 {
 
@@ -21,3 +36,4 @@ public class Ex57 {
 	}
 
 }
+*/

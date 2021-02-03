561fcfb18185e7db8965b1e2e1e9be1bbfcfee90
ljh468 <ljh468@naver.com>
2021-02-04 05:55:52 +0900

    210204

--- Codeup100/src/Ex59.java
+++ Codeup100/src/Ex59.java
@@ -1,24 +1,21 @@
 
 import java.util.Scanner;
-//[기초-종합] 수 나열하기3
+//[기초-종합] 수 나열하기2
 public class Ex59{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
-        int a = s.nextInt();
-        int b = s.nextInt();
-        int c = s.nextInt();
-        int n = s.nextInt();
-        long res = a * b + c;
-        
-        if(n == 1)
-        	System.out.println(a);
-        else {
-        	for(int i = 2; i < n; i++) {
-        		res = res * b + c;
-        	}
-        	System.out.println(res);
+
+        long a = s.nextLong();
+        long r = s.nextLong();
+        long n = s.nextLong();
+
+        long result = a;
+
+        for (int i = 1; i < n; i++) {
+            result *= r;
         }
-        
+
+        System.out.println(result);
+
     }
 }
--- Codeup100/src/Ex60.java
+++ Codeup100/src/Ex60.java
@@ -1,21 +1,23 @@
-
 import java.util.Scanner;
-//[기초-종합] 수 나열하기2
+//[기초-종합] 수 나열하기3
 public class Ex60{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
-        long a = s.nextLong();
-        long r = s.nextLong();
-        long n = s.nextLong();
-
-        long result = a;
-
-        for (int i = 1; i < n; i++) {
-            result *= r;
+        
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        int n = s.nextInt();
+        long res = a * b + c;
+        
+        if(n == 1)
+        	System.out.println(a);
+        else {
+        	for(int i = 2; i < n; i++) {
+        		res = res * b + c;
+        	}
+        	System.out.println(res);
         }
-
-        System.out.println(result);
-
+        
     }
 }

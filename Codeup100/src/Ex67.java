b7b031eb33aeba54d80e217567ec41e456358bbb
ljh468 <ljh468@naver.com>
2021-03-04 23:04:24 +0900

    0304

--- Codeup100/src/Ex67.java
+++ Codeup100/src/Ex67.java
@@ -3,7 +3,45 @@ import java.util.Scanner;
 
 public class Ex67 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
 
-    }//end of main
+        int sizeX = sc.nextInt();
+        int sizeY = sc.nextInt();
+
+        // 배열 생성
+        int[][] arr = new int[sizeX][sizeY];
+
+        int n = sc.nextInt();
+        int l, d, x, y;
+
+        // 갯수 만큼 반복
+        for (int i = 0; i < n; i++) {
+            l = sc.nextInt();
+            d = sc.nextInt();
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            // 막대의 가로 세로 구분
+            if (d == 0) {
+                for (int a = y; a < y + l; a++) {
+                    arr[x - 1][a - 1] = 1;
+                }
+            } else {
+                for (int a = x; a < x + l; a++) {
+                    arr[a - 1][y - 1] = 1;
+                }
+            }
+
+        }
+
+        // 배열 출력
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
 }

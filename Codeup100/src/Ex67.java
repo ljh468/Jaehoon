f61ed01a85f8aedb268d445dd39c12fb2bae175b
ljh468 <ljh468@naver.com>
2021-03-05 00:04:01 +0900

    210305

--- Codeup100/src/Ex67.java
+++ Codeup100/src/Ex67.java
@@ -1,47 +1,45 @@
-//  1098 : [기초-2차원배열] 설탕과자 뽑기
 import java.util.Scanner;
 
 public class Ex67 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
 
-        int sizeX = sc.nextInt();
-        int sizeY = sc.nextInt();
-
-        // 배열 생성
-        int[][] arr = new int[sizeX][sizeY];
-
-        int n = sc.nextInt();
-        int l, d, x, y;
-
-        // 갯수 만큼 반복
-        for (int i = 0; i < n; i++) {
-            l = sc.nextInt();
-            d = sc.nextInt();
-            x = sc.nextInt();
-            y = sc.nextInt();
-
-            // 막대의 가로 세로 구분
-            if (d == 0) {
-                for (int a = y; a < y + l; a++) {
-                    arr[x - 1][a - 1] = 1;
-                }
-            } else {
-                for (int a = x; a < x + l; a++) {
-                    arr[a - 1][y - 1] = 1;
-                }
-            }
-
-        }
-
-        // 배열 출력
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-    }
-
+		Scanner scan = new Scanner(System.in);
+		
+		int w = scan.nextInt();
+		int h = scan.nextInt();
+		int count = scan.nextInt();
+		
+		int[][] plate = new int[w][h];
+		for (int i = 0; i < count; i++) {
+			
+			int l = scan.nextInt();
+			int d = scan.nextInt();
+			int x = scan.nextInt() - 1;
+			int y = scan.nextInt() - 1;
+			
+			
+			for (int j = 0; j < l; j++) {
+				if (d==0) {
+					if (y-1+j < 100-h) {
+						plate[x][y+j] = 1;						
+					}
+				} else {
+					if (x-1+j < 100-w) {
+						plate[x+j][y] = 1;						
+					}
+				}
+			}
+						
+		}
+		
+		for (int a = 0; a < w; a++) {
+			for (int b = 0; b < h; b++) {
+				System.out.printf("%d ", plate[a][b]);
+			}
+			System.out.println();
+		}
+		
+	}
+	
 }
